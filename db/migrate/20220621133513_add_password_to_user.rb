e1aed2b4bf7d60dbd8a9c38ddad757971e87a579
Brad Bell <brad.bb.bell@gmail.com>
2022-07-28 11:35:00 -0600

    Adds users to top of seeds file

--- db/migrate/20220621133513_add_password_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddPasswordToUser < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :password, :string
-    add_column :users, :password_confirmation, :string
-  end
-end
--- db/migrate/20220621134246_remove_passwordfrom_user.rb
+++
@@ -1,6 +0,0 @@
-class RemovePasswordfromUser < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :password, :string
-    remove_column :users, :password_confirmation, :string
-  end
-end
