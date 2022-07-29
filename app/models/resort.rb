c8536c9cbf09be5e1d5490444110b800b56df19f
Brad Bell <brad.bb.bell@gmail.com>
2022-07-29 11:55:08 -0600

    uncomments user and resort associations

--- app/models/resort.rb
+++ app/models/resort.rb
@@ -1,5 +1,5 @@
 class Resort < ApplicationRecord
-  # belongs_to :user
+  belongs_to :user
   has_many :events
   has_many :conditions_reports
   has_many :favorite_resorts, class_name: "Resort", foreign_key: "resort_id"
